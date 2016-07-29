3ce5298246a2d8052a8c5b2528f0945a0993f9e2
Mzapeka <mzapeka@gmail.com>
2016-07-30 00:33:21 +0300

    Update Dealer.php

--- core/View/Dealer.php
+++ core/View/Dealer.php
@@ -13,42 +13,6 @@ use System\View;
 
 class Dealer extends View {
 
-    private function menu($item){
-        include_once("View/dealer/menu.php");
-    }
-
-    public function showDealerInterface($info){
-        $this->loadHeader();
-        $this->menu(1);
-        include_once("View/dealer/mainForm.php");
-        $this->loadfooter();
-    }
-    public function showUsers($data){
-        $this->loadHeader();
-        $this->menu(2);
-        include_once("View/dealer/customers.php");
-        $this->loadfooter();
-    }
-
-    public function userRegForm(){
-        $this->loadHeader();
-        include_once("View/dealer/userRegForm.php");
-        $this->loadfooter();
-    }
-
-    public function addDevForm(){
-        $this->loadHeader();
-        include_once("View/dealer/devAddForm.php");
-        $this->loadfooter();
-    }
-
-	public function showSettings() {
-		$this->loadHeader();
-        $this->menu(3);
-		echo "Данный раздел находится в разработке";
-		$this->loadfooter();
-	}
-
 
 
 }
